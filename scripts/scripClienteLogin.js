662ff2c9dafa9dbe232036d015ca5d539634c701
LimaBernardo212 <mariahelenalima911@gmail.com>
2024-11-18 18:15:58 -0300

    18.11.24

--- scripts/scripClienteLogin.js
+++ scripts/scripClienteLogin.js
@@ -4,6 +4,8 @@ const evereas = document.querySelector("#eyereas");
 const sobpinc = document.querySelector("#sobpinc")
 var everImageTrocar = "images/olho.png";
 var everImage = "images/olho (1).png";
+var everImageTrocar1 = "images/olho.png";
+var everImage1 = "images/olho (1).png";
 eyere.addEventListener("click", function (){
     pinc.type = pinc.type === "password" ? "text" : "password";
     document.getElementById("Everelson").src = everImage;
@@ -13,8 +15,8 @@ eyere.addEventListener("click", function (){
 })
 evereas.addEventListener("click", function (){
     sobpinc.type = sobpinc.type === "password" ? "text" : "password";
-    document.getElementById("Evereia").src = everImage;
-    let aux = everImageTrocar
-    everImageTrocar = everImage
-    everImage = aux
+    document.getElementById("Evereia").src = everImage1;
+    let auxi = everImageTrocar1
+    everImageTrocar1 = everImage1
+    everImage1 = auxi
 })
--- scripts/scriptProgrammerLogin.js
+++ scripts/scriptProgrammerLogin.js
@@ -4,6 +4,8 @@ const ever = document.querySelector("#ever");
 const sobpin = document.querySelector("#sobpin");
 var everImageTrocar = "images/olho.png";
 var everImage = "images/olho (1).png";
+var everImageTrocar1 = "images/olho.png";
+var everImage1 = "images/olho (1).png";
 eyer.addEventListener("click", function (){
     pin.type = pin.type === "password" ? "text" : "password";
     document.getElementById("everIMg").src = everImage;
@@ -13,8 +15,8 @@ eyer.addEventListener("click", function (){
 })
 ever.addEventListener("click", function (){
     sobpin.type = sobpin.type === "password" ? "text" : "password";
-    document.getElementById("Everelson").src = everImage;
-    let aux = everImageTrocar
-    everImageTrocar = everImage
-    everImage = aux
+    document.getElementById("EverForever").src = everImage1;
+    let auxi = everImageTrocar1
+    everImageTrocar1 = everImage1
+    everImage1 = auxi
 })
